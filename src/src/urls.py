6bb02ce3a53280b087587dc5f79306e0e09e926a
iamsaicharan <charanvadakapur@gmail.com>
2022-01-20 19:42:29 +0530

    final changes before uploading it to elasticbeamstalk

--- src/src/urls.py
+++ src/src/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path("", index, name="landing-page"),
     path('pages/', include('pages.urls')),
 ]
